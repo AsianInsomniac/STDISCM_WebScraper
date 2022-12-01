8cfe34d5ed7997efb3e473af42114bc5c8fc8c2e
Angelo Gregorio <g782065@gmail.com>
2022-12-01 11:40:30 +0800

    Basic Implementation of File I/O
    Also updated .gitignore to ignore output files

---
+++ STDISCM_WebScraper.py
@@ -0,0 +1,28 @@
+import csv
+
+class webScraper():
+    pass
+
+class file():
+    def csvOutput():
+        header = ["Email", "Name", "Office", "Department/Unit"]
+
+        with open("output.csv", "w") as csvFile:
+            csvWriter = csv.writer(csvFile)
+            csvWriter.writerow(header)
+
+        pass
+
+    def txtOutput(url):
+        with open("output.txt", "w") as txtFile:
+            txtFile.write("URL: " + url + "\nNumber of pages scraped: " + "\nNumber of email addresses found: ")
+
+if __name__=="__main__":
+    url = input("URL: ")
+    nTime = input("Scraping Time (minutes): ")
+    
+    # OPTIONAL: Number of threads/processes to be used
+    nThread = input("# of Threads/Processes to be used: ")
+
+    file.txtOutput(url)
+    file.csvOutput()
