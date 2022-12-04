2b11daeaa07c85d9941d665d00099054353ac5de
Angelo Gregorio <g782065@gmail.com>
2022-12-04 08:27:01 +0800

    Basic Implementation of CSV Formatting and Web Scraping
    Test data consists of 1 row of email and name from a singular web page

--- STDISCM_WebScraper.py
+++ STDISCM_WebScraper.py
@@ -1,37 +1,89 @@
 import csv
+from datetime import datetime
+from dataclasses import dataclass
+from urllib.request import urlopen
+from urllib.request import Request
+from bs4 import BeautifulSoup
+import pandas as pd
 
 class webScraper():
-    pass
+    def getData(url):
+        # Arrays for Data
+        @dataclass
+        class webEntry:
+            email: str
+            name: str
+        
+        entries = []
+
+        emails = []
+        names = []
+
+        #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        # driver.get(url)
+        # content = driver.page_source
+        user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+        headers = {'User-Agent':user_agent,} 
+
+        request = Request(url, None, headers)
+        content = urlopen(request).read()
+        soup = BeautifulSoup(content, "html.parser")
+        
+        for email in soup.find_all('a', attrs={"class": "btn btn-sm btn-block text-capitalize"}):
+            emailStr = email.get('href')
+            emailStr = emailStr.replace('mailto:', '')
+            emails.append(emailStr)
+        for name in soup.find_all('h3'):
+            names.append(name.text)
+
+        file.csvOutput(emails, names, url)
 
 class file():
-    def csvOutput():
-        header = ["Email", "Name", "Office", "Department/Unit"]
+    def csvOutput(emails, names, url):
+        emailCount = 0
+        header = ["Email", "Name/Office/Department/Unit"]
 
-        with open("output.csv", "w") as csvFile:
-            csvWriter = csv.writer(csvFile)
-            csvWriter.writerow(header)
+        with open("output.csv", "w", newline = '') as csvFile:
+            csvWriter = csv.DictWriter(csvFile, fieldnames = header)
+            csvWriter.writeheader()
+            
+            for email, name in zip(emails, names):
+                print(email + ' | ' + name)
+                csvWriter.writerow({'Email': email, 'Name/Office/Department/Unit': name})
+                emailCount += 1
 
-        pass
+        file.txtOutput(url, emailCount)
 
-    def txtOutput(url):
+    def txtOutput(url, emailCount):
         with open("output.txt", "w") as txtFile:
-            txtFile.write("URL: " + url + "\nNumber of pages scraped: " + "\nNumber of email addresses found: ")
+            txtFile.write("URL " + url + "\nNumber of pages scraped: " + "\nNumber of email addresses found: " + str(emailCount))
 
 if __name__=="__main__":
     url = "https://www.dlsu.edu.ph"
 
     while True:
-        urlInput = input("URL: ")
+        urlInput = input("URL (https): ")
 
-        if(urlInput != url):
-            print("Only the DLSU website can be used for this project.\n")
+        if(url not in urlInput):
+            print("Only the DLSU website can be used for this project.")
         else:
             break
     
-    nTime = input("Scraping Time (minutes): ")
+    while True:
+        nTime = input("Scraping Time (minutes): ")
+
+        if(not nTime.isnumeric()):
+            print("Only integer inputs are accepted.")
+        else:
+            break
     
     # OPTIONAL: Number of threads/processes to be used
-    nThread = input("# of Threads/Processes to be used: ")
+    while True:
+        nThread = input("# of Threads/Processes to be used: ")
 
-    file.txtOutput(url)
-    file.csvOutput()
+        if(not nThread.isnumeric()):
+            print("Only integer inputs are accepted.")
+        else:
+            break
+
+    webScraper.getData(urlInput)
